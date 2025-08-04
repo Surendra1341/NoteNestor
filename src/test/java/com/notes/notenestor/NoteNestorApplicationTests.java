56db1aa9b1c197b6e7bc3e793e6f4b0ab120600b
Surendra1341 <kr494167@gmail.com>
2025-08-04 10:13:43 +0530

    "Initial commit"

---
+++ src/test/java/com/notes/notenestor/NoteNestorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notes.notenestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteNestorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
