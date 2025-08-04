773fc6130462ee6062a7481bd8998da93493b36c
Surendra1341 <kr494167@gmail.com>
2025-08-04 12:45:58 +0530

    "Categories updates"

---
+++ src/main/java/com/notes/notenestor/entity/Category.java
@@ -0,0 +1,28 @@
+package com.notes.notenestor.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Category extends BaseModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    private String description;
+
+
+}
