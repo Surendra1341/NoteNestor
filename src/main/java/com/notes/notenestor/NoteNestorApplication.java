10113aaeef1bea0d3f5522f9f6a3f7186d1b8b8e
Surendra1341 <kr494167@gmail.com>
2025-08-04 10:13:43 +0530

    "Initial commit"

---
+++ src/main/java/com/notes/notenestor/NoteNestorApplication.java
@@ -0,0 +1,13 @@
+package com.notes.notenestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteNestorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteNestorApplication.class, args);
+    }
+
+}
