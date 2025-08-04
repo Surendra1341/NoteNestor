9880cc29fe255b47a15afc50e2fb0a3bf76c835a
Surendra1341 <kr494167@gmail.com>
2025-08-04 18:05:26 +0530

    "validation add"

--- src/main/java/com/notes/notenestor/dto/CategoryDto.java
+++ src/main/java/com/notes/notenestor/dto/CategoryDto.java
@@ -1,5 +1,9 @@
 package com.notes.notenestor.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,10 +18,17 @@ public class CategoryDto {
 
     private Integer id;
 
+    @NotBlank
+    @Min(value = 1)
+    @Max(value = 20)
     private String name;
 
+    @NotBlank
+    @Min(value = 1)
+    @Max(value = 100)
     private String description;
 
+    @NotNull
     private Boolean isActive;
 
     private Integer createdBy;
